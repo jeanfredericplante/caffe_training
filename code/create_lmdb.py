9978592a1555733a52e6a1b10d0ab7a2250dd291
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2017-07-11 18:48:15 +0000

    modified the lmdb creation script to work with ones and tens files

--- code/create_lmdb.py
+++ code/create_lmdb.py
@@ -24,6 +24,9 @@ import lmdb
 IMAGE_WIDTH = 227
 IMAGE_HEIGHT = 227
 
+#Preprocessing images
+TRAINING_PERCENTAGE = 90
+
 def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
 
     #Histogram Equalization
@@ -52,26 +55,49 @@ validation_lmdb = '/workspace/blocks_training/deeplearning-cats-dogs-tutorial/in
 os.system('rm -rf  ' + train_lmdb)
 os.system('rm -rf  ' + validation_lmdb)
 
+ones_files = [img for img in glob.glob("../input/one/*.JPG")]
+tens_files = [img for img in glob.glob("../input/ten/*.JPG")]
 
-train_data = [img for img in glob.glob("../input/train/*jpg")]
-test_data = [img for img in glob.glob("../input/test1/*jpg")]
+# Selecting test and training data
+trainingsize_ones = TRAINING_PERCENTAGE * len(ones_files)
+trainingsize_tens = TRAINING_PERCENTAGE * len(tens_files)
+random.shuffle(ones_files)
+random.shuffle(tens_files)
 
-#Shuffle train_data
+# Training and testing arrays
+train_data = ones_files[:trainingsize_ones]+tens_files[:trainingsize_tens]
+test_data = ones_files[trainingsize_ones+1:]+tens_files[trainingsize_tens+1:]
 random.shuffle(train_data)
+random.shuffle(test_data)
 
+print '\n',len(ones_files)," number of ones images and ",len(tens_files), "number of tens images"
+print 'concatenated training data ', len(train_data)
+print 'concatenated test data ', len(test_data)
+#for in_idx, img_path in enumerate(train_data):
+#	if 'one' in img_path:
+#		print 'this is a one file'
+#	else: 
+#		print "this is a ten file"
+
+#train_data = [img for img in glob.glob("../input/train/*jpg")]
+#test_data = [img for img in glob.glob("../input/test1/*jpg")]
+#
+##Shuffle train_data
+#random.shuffle(train_data)
+#
 print 'Creating train_lmdb'
 
 in_db = lmdb.open(train_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
     for in_idx, img_path in enumerate(train_data):
-        if in_idx %  6 == 0:
+        if in_idx %  15 == 0: # don't have that much data
             continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'cat' in img_path:
-            label = 0
-        else:
+        if 'one' in img_path:
             label = 1
+        else:
+            label = 10
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
         print '{:0>5d}'.format(in_idx) + ':' + img_path
@@ -87,10 +113,10 @@ with in_db.begin(write=True) as in_txn:
             continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'cat' in img_path:
-            label = 0
-        else:
+        if 'one' in img_path:
             label = 1
+        else:
+            label = 10
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
         print '{:0>5d}'.format(in_idx) + ':' + img_path
